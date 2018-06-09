510be7fe4d943bb95afb8ff35eb1599813d72ea9
RedFantom <redfantom@outlook.com>
2018-06-09 23:05:59 +0200

    Work on changing to a universal OOP implementation

--- README.md
+++ README.md
@@ -7,7 +7,21 @@ as well, and maintaining multiple CI setups with different
 configurations got very annoying.
 
 That's why this repository contains a modified version of the `ci.py` I
-had written for the `ttkthemes` package. By using environment variables
-and custom hook files the behaviour can be adjusted for any Python 
-package, or that's the plan anyway.
+had written for the `ttkthemes` package. By using a simple ini
+configuration file that is read with a `ConfigParser`, custom
+dependencies can be installed (like system packages on Linux).
 
+While technically all of this is possible by using YAML configuration
+files, as used by AppVeyor and Travis-CI, I have found it to be 
+tedious to keep up to date with changes in both Travis-CI and various
+dependencies (like the `pip==10.0.0` update) for all of my repositories.
+
+## Usage
+The `ci.py` should be run by the test system. `ci.py` will automatically
+install dependencies defined in a `requirements.txt` in the root of 
+your repository, as well as any additional dependencies configured in
+the `ci.ini`, or `.ci.ini`, file.
+
+You can define your own platform matrix, but you can also build one with
+the help of the `platform.py` script. This will generate YAML-files for
+AppVeyor and Travis-CI alike.
--- ci.py
+++ ci.py
@@ -3,13 +3,14 @@ Author: RedFantom
 License: GNU GPLv3
 Copyright (c) 2017-2018 RedFantom
 """
-# Standard library imports only
-import sys
+from configparser import ConfigParser
+import logging
 import os
 from shutil import rmtree
+import sys
 
 DEPENDENCIES = ["pillow"]
-REQUIREMENTS = ["codecov", "coverage", "nose", "setuptools", "pip", "wheel"]
+REQUIREMENTS = ["codecov", "coverage", "nose", "setuptools", "pip", "wheel", "semantic_version"]
 PACKAGES = "python-tk python3-tk libtk-img"
 
 SDIST = os.environ.get("SDIST", "false") == "true"
@@ -30,16 +31,125 @@ def run_command(command):
         return os.WEXITSTATUS(return_info)
 
 
-def check_wheel_existence():
-    """Return True if a wheel is built"""
-    return len([file for file in os.listdir("dist") if file.endswith((".whl", ".tar.gz"))]) != 0
+class CI(object):
+    """
+    Manages a CI instance on AppVeyor or Travis-CI for a Python project
+
+    Configuration of this class should happen in the `ci.ini`, or
+    alternatively `.ci.ini`, file. This class will automatically detect
+    the platform that is being run on and install the packages
+    appropriately.
+    """
+
+    TRAVIS_CI = "travis"
+    APPVEYOR = "appveyor"
+
+    WINDOWS = "windows"
+    LINUX = "linux"
+    MACOS = "macos"
+
+    CONFIG = ["ci.ini", ".ci.ini"]
+
+    def __init__(self, platform=None):
+        """
+        :param platform: Optional platform override
+        """
+        self.logger = self.setup_logger()
+        self.platform = platform if platform is not None else self.get_platform()
+        self.os = self.get_os()
+        self.config = ConfigParser()
+        self.read_config()
+
+    def build_wheel(self):
+        """Build a wheel from a package"""
+        pass
+
+    def read_config(self):
+        """Read the configuration file from disk"""
+        path = self.get_config_path()
+        if path is None:
+            raise FileNotFoundError("Configuration file could not be found")
+        self.config.read_file(path)
+
+    @staticmethod
+    def get_built_package_exists():
+        """Return whether the wheel or sdist has been built and exists"""
+        return len([file for file in os.listdir("dist") if file.endswith((".whl", ".tar.gz"))]) != 0
+
+    @staticmethod
+    def pip_install(pkgs: list):
+        """Install a list of packages with pip"""
+
+        # Import pip
+        from pip import __version__ as pip_version
+        if Version(pip_version) >= Version("10.0.0"):
+            import pip._internal as pip
+        else:
+            import pip
+
+        # Install/upgrade the specified packages
+        command = ["install", "-U"] + pkgs
+        pip.main(command)
+
+    @staticmethod
+    def get_config_path():
+        """Return the absolute path to the config file if it exists"""
+        for file in CI.CONFIG:
+            if not os.path.exists(file):
+                continue
+            return os.path.abspath(file)
+        return None
+
+    @staticmethod
+    def get_platform():
+        """Return the platform that is being run on"""
+        if os.environ.get("TRAVIS", "false") == "true":
+            return CI.TRAVIS_CI
+        elif os.environ.get("APPVEYOR", "false") in ("True", "true"):
+            # true on Windows, True on Ubuntu
+            return CI.APPVEYOR
+        else:
+            raise RuntimeError("This platform is not currently supported")
+
+    @staticmethod
+    def get_os():
+        """Return the OS that is being run on"""
+        if sys.platform == "win32":
+            return CI.WINDOWS
+        elif "linux" in sys.platform:  # `linux2` under Python 2
+            return CI.LINUX
+        elif sys.platform == "darwin":
+            return CI.MACOS
+        else:
+            raise RuntimeError("Unsupported OS: {}".format(sys.platform))
+
+    @staticmethod
+    def setup_logger():
+        """Return a stdout logger"""
+        logger = logging.Logger("CI")
+        stdout = logging.StreamHandler(sys.stdout)
+        fmt = logging.Formatter("%(levelname)s - %(message)s")
+        stdout.setFormatter(fmt)
+        logger.addHandler(stdout)
+        return logger
+
+
+class Version(object):
+    """Parses a semantic version string."""
+    def __init__(self, string):
+        """
+        :param string: semantic version string (major.minor.patch)
+        """
+        self.major, self.minor, self.patch = map(int, string.split("."))
+        self.version = (self.major, self.minor, self.patch)
+
+    def __ge__(self, other):
+        return all(elem1 >= elem2 for elem1, elem2 in zip(self.version, other.version))
 
 
 def ci(python="python", codecov="codecov", coverage_file="coverage.xml"):
     """Run the most common CI tasks"""
-    # Upgrade pip and setuptools and install dependencies
-    import pip
-    pip.main(["install"] + DEPENDENCIES + REQUIREMENTS + ["-U"])
+
     # Build the installation wheel
     dist_type = "bdist_wheel" if not SDIST else "sdist"
     return_code = run_command("{} setup.py {}".format(python, dist_type))
@@ -47,6 +157,7 @@ def ci(python="python", codecov="codecov", coverage_file="coverage.xml"):
         print("Building and installing wheel failed.")
         exit(return_code)
     assert os.path.exists(os.path.join("ttkthemes", "tkimg"))
+
     # Check if an artifact exists
     assert check_wheel_existence()
     print("Wheel file exists.")
@@ -59,6 +170,7 @@ def ci(python="python", codecov="codecov", coverage_file="coverage.xml"):
         print("Installation of wheel failed.")
         exit(return_code)
     print("Wheel file installed.")
+
     # Remove all non-essential files
     for to_delete in TO_DELETE:
         rmtree(to_delete)
@@ -103,7 +215,7 @@ def ci_macos():
 
 
 def ci_linux():
-    """Setup Travis-CI linux for installation, testing and wheels"""
+    """Setup Travis-CI linux for installation and testing"""
     run_command("sudo apt-get install {}".format(PACKAGES))
     ci()
 
@@ -118,3 +230,4 @@ if __name__ == '__main__':
         ci_macos()
     else:
         raise RuntimeError("Invalid platform: ", sys.platform)
+
